1a7f86fcdb4bc670b608c36453bc08b7be2d452b
CLÁUDIO MENDES <CLUDIO MENDES>
2022-11-07 09:48:44 -0300

    Módulo do Crispy-forms funcionando na página investimentos.html

--- projeto_invista_me/projeto_invista_me/settings.py
+++ projeto_invista_me/projeto_invista_me/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'invista_me'
+    'invista_me',
+    'crispy_forms'
     
     
 ]
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

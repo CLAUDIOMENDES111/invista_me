2ccad6b12556fa51fff98909bf9787037bb172c2
CLÁUDIO MENDES <CLUDIO MENDES>
2022-11-08 07:55:43 -0300

    DECLARANDO O NOVO APP USUARIOS

--- projeto_invista_me/projeto_invista_me/settings.py
+++ projeto_invista_me/projeto_invista_me/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'invista_me',
-    'crispy_forms'
-    
+    'crispy_forms',
+    'usuarios'   
     
 ]
 
